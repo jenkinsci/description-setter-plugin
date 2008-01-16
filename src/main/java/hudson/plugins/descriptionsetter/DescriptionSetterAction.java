e70140971d42a9e1be4991272622274af2a44aad
huybrechts <huybrechts@dev.java.net>
2008-01-16 19:47:17 +0000

    forgot some files...

--- src/main/java/hudson/plugins/descriptionsetter/DescriptionSetterAction.java
+++ src/main/java/hudson/plugins/descriptionsetter/DescriptionSetterAction.java
@@ -5,20 +5,14 @@ import hudson.model.Action;
 public class DescriptionSetterAction implements Action {
 
 	private static final long serialVersionUID = 1L;
+	private final String description;
 
-	private final String version;
-
-	public DescriptionSetterAction(String version) {
-		super();
-		this.version = version;
-	}
-
-	public String getVersion() {
-		return version;
+	public DescriptionSetterAction(String description) {
+		this.description = description;
 	}
 
 	public String getDisplayName() {
-		return "Release Version";
+		return "Description";
 	}
 
 	public String getIconFileName() {
@@ -26,6 +20,10 @@ public class DescriptionSetterAction implements Action {
 	}
 
 	public String getUrlName() {
-		return "release-version";
+		return "description";
+	}
+
+	public String getDescription() {
+		return description;
 	}
 }
